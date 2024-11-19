a38d1991cabb7abe365b31ef32eed0d172b67b01
Bruno Vilardi Bueno <brunovilardibueno@gmail.com>
2024-11-19 15:34:23 -0300

    remove KB configuration from lambda

--- iac/setup_kb_lambda/setup_kb.py
+++ iac/setup_kb_lambda/setup_kb.py
@@ -57,37 +57,6 @@ def setup_rds(database_name: str, host: str, user: str, password: str, port: str
     print("Setup rds for bedrock kb completed")
     conn.close()
 
-def setup_bedrock_kb(credentials_secret_arn: str,
-                     database_name: str, aurora_cluster_arn: str, table_name: str):
-    # Create Bedrock Knowledgebase
-    bedrock_client = boto3.client("bedrock-agent")
-    bedrock_client.create_knowledge_base(
-        name="ChocolateFactoryKB",
-        description="Knowledge base for Chocolate Factory",
-        schema={
-            "id": "uuid",
-            "embedding": "vector(1024)",
-            "chunks": "text",
-            "metadata": "json"
-        },
-        index="hnsw",
-        knowledgeBaseConfiguration={
-            'rdsConfiguration': {
-                'credentialsSecretArn': credentials_secret_arn,
-                'databaseName': database_name,
-                'fieldMapping': {
-                    'metadataField': 'metadata',
-                    'primaryKeyField': 'uuid',
-                    'textField': 'chunks',
-                    'vectorField': 'embedding'
-                },
-                'resourceArn': aurora_cluster_arn,
-                'tableName': table_name
-            }
-        }
-
-    )
-
 def lambda_handler(event, context):
     # Your post-deployment logic here
     print("Post-deployment trigger executed")
@@ -108,13 +77,6 @@ def lambda_handler(event, context):
                 user=resource_properties["USER"],
                 password=json.loads(get_secret(resource_properties.get('SECRET_NAME')))['password'],
                 port=resource_properties["PORT"]
-            )
-
-            setup_bedrock_kb(
-                credentials_secret_arn=resource_properties["SECRET_NAME"],
-                database_name=resource_properties["DATABASE_NAME"],
-                aurora_cluster_arn=resource_properties["HOST"],
-                table_name="bedrock_integration.bedrock_kb"
             )
 
             token = ("%s.%s" % (id_generator(6), id_generator(16)))
